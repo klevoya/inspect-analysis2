9d27995efb79b80aed445c90a23ca8d5d08b09fc
rex <jshe9611@plang8.cs.usyd.edu.au>
2020-09-24 18:03:29 +1000

    add require_auth pattern prototype

--- benchmark/rollback/rollback.cpp
+++ benchmark/rollback/rollback.cpp
@@ -8,21 +8,14 @@ class Rollback : public contract {
   public:
       using contract::contract;
 
-      void reveal(std::string memo, name from, asset quantity){
-        if(memo == "12345"){
-          action(
+      [[eosio::on_notify("eosio.token::transfer")]] 
+      void playgame(name from, name to, asset quantity, std::string memo) {
+        if(to != get_self()) return;
+        action(
            permission_level{get_self(), "active"_n},
            "eosio.token"_n, "transfer"_n,
            std::make_tuple(get_self(), from, quantity, std::string(""))
           ).send();
-        }
-      }
-
-
-      [[eosio::on_notify("eosio.token::transfer")]] 
-      void playgame(name from, name to, asset quantity, std::string memo) {
-        if(to != get_self()) return;
-        reveal(memo, from, quantity);
       }
 
 };
