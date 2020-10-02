fb5e013e5273e6c2f38681e7e2cf1c38051e6725
rex <jshe9611@plang8.cs.usyd.edu.au>
2020-10-02 15:15:44 +1000

    fix memeory load

---
+++ benchmark/require_auth/no_auth_remove.cpp
@@ -0,0 +1,27 @@
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+
+using namespace eosio;
+
+
+class NoAuthRemove : public contract {
+  public:
+      using contract::contract;
+      [[eosio::action]]
+      void transfer(name user) {
+        people_index people(get_first_receiver(), get_first_receiver().value);
+
+        require_auth(user);
+        auto itr = people.find(user.value);
+        people.erase( itr );
+      }
+  private:
+  	  struct [[eosio::table]] person {
+  	    name key;
+  	    uint64_t primary_key() const { return key.value; }
+  	  };
+  	  using people_index = eosio::multi_index<"people"_n, person>;
+};
+
+
+EOSIO_DISPATCH( NoAuthRemove, (transfer));
