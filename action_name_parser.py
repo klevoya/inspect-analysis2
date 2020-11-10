531105e7a1113b842abc07bc0b7c1418c80fc741
Rex Shen <rex0481775250@gmail.com>
2020-11-10 17:34:53 +1100

    add more on action param

--- action_name_parser.py
+++ action_name_parser.py
@@ -1,12 +1,13 @@
 
+max_len = 12
 def string_from_long(action):
     charmap = ['.','1','2','3','4','5','a','b','c','d','e','f','g','h','i',
                 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    str = [None] * 13
+    str = [None] * (max_len + 1)
     tmp = int(float(action))
-    for i in range(13):
+    for i in range(max_len + 1):
         c = charmap[int(tmp & (0x0f if i == 0 else 0x1f))]
-        str[12-i] = c
+        str[max_len-i] = c
         tmp >>= (4 if i == 0 else 5)
     out_str = ''.join(str)
     return out_str.split('.', 1)[0]
@@ -15,13 +16,13 @@ def string_from_long(action):
 def string_to_long(name):
     n = 0
     i = 0
-    while i < len(name) and i < 12:
+    while i < len(name) and i < max_len:
         tmp = char2symbol(name[i])
         n |= (tmp & 0x1f) << (64 - 5 * (i + 1))
         i += 1
 
-    if i == 12:
-        n |= char2symbol(name[12]) & 0x0F
+    if i == max_len:
+        n |= char2symbol(name[max_len]) & 0x0F
 
     return n
 
