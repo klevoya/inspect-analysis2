46b0702affeb1ac42e7e31029b4c47d14da22ee7
Rex Shen <rex0481775250@gmail.com>
2020-12-09 19:56:00 +1100

    add store as copy assignment & can store loaded data now

--- benchmark/fake_notify/fake_notify.cpp
+++ benchmark/fake_notify/fake_notify.cpp
@@ -10,7 +10,7 @@ CONTRACT fake_notify : public contract {
   public:
       using contract::contract;
 
-      // no checking from or to
+      // no checking from or to ?
       [[eosio::on_notify("eosio.token::transfer")]] void playgame(name from, name to, asset quantity, string memo) {
         if(from == get_self()) return;
 
